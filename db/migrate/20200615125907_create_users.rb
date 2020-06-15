866df75a8b20c1695ebf833a8d9df9761c08fc94
DarkLaughter <ramon.l.echeverria@gmail.com>
2020-06-15 12:48:59 -0400

    events api initial build

---
+++ db/migrate/20200615125907_create_users.rb
@@ -0,0 +1,14 @@
+class CreateUsers < ActiveRecord::Migration[6.0]
+  def change
+    create_table :users do |t|
+      t.string :name
+      t.string :contact
+      t.string :username
+      t.string :password_digest
+      t.string :img_url
+      t.string :bio
+
+      t.timestamps
+    end
+  end
+end
---
+++ db/seeds.rb
@@ -0,0 +1,30 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
+puts "creating users"
+User.create(name: "Brandon", contact: "brandon@", username:"brandonk", password:"pass123")
+User.create(name: "Ramon", contact: "ramon@", username:"ramone", password:"pass123")
+    
+35.times do 
+    User.create(name: Faker::Name.name, contact: Faker::Internet.safe_email, username: Faker::Internet.username, password:"pass123", img_url: Faker::Avatar.image, bio:Faker::Quote.famous_last_words)
+end 
+
+puts "creating events"
+
+10.times do
+    Event.create(title: Faker::WorldCup.stadium, date: Faker::Date.forward(days: 14) , content: Faker::Hipster.sentences )
+    Event.create(title: Faker::Esport.event, date: Faker::Date.forward(days: 14) , content: Faker::Hipster.sentences )
+    Event.create(title: Faker::University.name , date: Faker::Date.forward(days: 14) , content: Faker::Hipster.sentences )
+    Event.create(title: Faker::Restaurant.name, date: Faker::Date.forward(days: 14) , content: Faker::Hipster.sentences )
+end
+
+puts "creating user_events"
+
+50.times do
+    UserEvent.create(user_id: User.all.sample.id, event_id: Event.all.sample.id)
+end

3465086c4dac57a28d72536d22a465fa62792587
byoon97 <byoon5397@gmail.com>
2020-06-16 15:43:53 -0400

    check

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,7 +14,7 @@ puts "creating users"
 User.create(name: "Brandon", contact: "brandon@", username:"brandonk", password:"pass123")
 User.create(name: "Ramon", contact: "ramon@", username:"ramone", password:"pass123")
     
-35.times do 
+5.times do 
     User.create(name: Faker::Name.name, contact: Faker::Internet.safe_email, username: Faker::Internet.username, password:"pass123", img_url: Faker::Avatar.image, bio:Faker::Quote.famous_last_words)
 end 
 
@@ -23,12 +23,10 @@ puts "creating events"
 10.times do
     Event.create(title: Faker::WorldCup.stadium, date: Faker::Date.forward(days: 14) , content: Faker::Hipster.sentence )
     Event.create(title: Faker::Esport.event, date: Faker::Date.forward(days: 14) , content: Faker::Hipster.sentence )
-    Event.create(title: Faker::University.name , date: Faker::Date.forward(days: 14) , content: Faker::Hipster.sentence )
-    Event.create(title: Faker::Restaurant.name, date: Faker::Date.forward(days: 14) , content: Faker::Hipster.sentence )
 end
 
 puts "creating user_events"
 
-50.times do
+30.times do
     UserEvent.create(user_id: User.all.sample.id, event_id: Event.all.sample.id)
 end

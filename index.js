174392e7c750856fc8d0f752ca023e101bfc81a2
DarkLaughter <ramon.l.echeverria@gmail.com>
2020-06-16 17:20:21 -0400

    form and misc.updates

--- index.js
+++ index.js
@@ -46,10 +46,10 @@ document.addEventListener('DOMContentLoaded', function(e){
 
         const addUserToEventForm = document.createElement('form')
             addUserToEventForm.innerHTML = `
-                <label style="color:blue">Attend This Event</label> 
-                <label>Username: </label>
-                <input type="text name="Username">
-                <label>Name: </label>
+                <label style="color:blue">Attend This Event</label></br> 
+                <label style="color:blue">Username: </label>
+                <input stype="text name="Username">
+                <label style="color:blue">Name: </label>
                 <input type="text name="Name">
                 <input type="submit" value="RSVP">
                 `
